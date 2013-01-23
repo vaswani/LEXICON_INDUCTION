edf3945e4541573d83824d90669ce1f611596dd6
Ashish Vaswani <vaswani@usc.edu>
2013-01-23 14:28:46 -0800

    added scripts so you can look at the k-nearest neighbors of a word in embeddings space

---
+++ SCRIPTS/checkEmbeddings.py
@@ -0,0 +1,26 @@
+import sys
+import cPickle as pickle
+from priority_dict import *
+import numpy
+
+
+#embeddings_dictionary = pickle.load(open(sys.argv[1]))
+embeddings_dictionary = dict((line.strip().split()[0],numpy.array(map(float,line.strip().split()[1:])))for line in open(sys.argv[1]))
+#print embeddings_dictionary
+word = sys.argv[2]
+if word not in embeddings_dictionary:
+    print 'not in embeddings_dictionary'
+    sys.exit()
+word_embeddings = embeddings_dictionary[word]
+distances = {}
+for i in embeddings_dictionary:
+    if i == word:
+        continue
+    distances[i] = numpy.square(word_embeddings-embeddings_dictionary[i]).sum()
+distances_heap = priority_dict(distances)
+
+
+for i in range(10):
+    p_argmin,p_min = distances_heap.pop_smallest()
+    print p_argmin,', distance:',p_min
+
