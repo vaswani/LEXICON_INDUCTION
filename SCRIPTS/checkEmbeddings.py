5a4f2456ad9c1c623bf1127b558da70cad19462b
Ashish Vaswani <vaswani@usc.edu>
2013-01-23 14:29:58 -0800

    added scripts so you can look at the k-nearest neighbors of a word in embeddings space

--- SCRIPTS/checkEmbeddings.py
+++ SCRIPTS/checkEmbeddings.py
@@ -8,6 +8,7 @@ import numpy
 embeddings_dictionary = dict((line.strip().split()[0],numpy.array(map(float,line.strip().split()[1:])))for line in open(sys.argv[1]))
 #print embeddings_dictionary
 word = sys.argv[2]
+k = int(sys.argv[3])
 if word not in embeddings_dictionary:
     print 'not in embeddings_dictionary'
     sys.exit()
@@ -20,7 +21,7 @@ for i in embeddings_dictionary:
 distances_heap = priority_dict(distances)
 
 
-for i in range(10):
+for i in range(k):
     p_argmin,p_min = distances_heap.pop_smallest()
     print p_argmin,', distance:',p_min
 
