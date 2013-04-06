31ce160f7bb0c5dd7d9ec2d745feb598e445b7fa
Ashish Vaswani <vaswani@usc.edu>
2013-04-05 19:19:59 -0700

    slight change in checkSymmetry.py script

--- Updated_code/checkSymmetry.py
+++ Updated_code/checkSymmetry.py
@@ -8,7 +8,9 @@ for line in open(sys.argv[1]):
     counts.append(map(int,line.split()[1:]))
 
 arr = numpy.array(counts)
-print arr.shape
-print (arr.transpose()==arr).prod()
+print 'context feature matrix has shape', arr.shape
+if (arr.transpose()==arr).prod() == 1:
+    print 'the context feature matrix is symmetric'
+
 #print print 'symmetric',(arr.transpose(1, 0, 2) == arr).all()
 
