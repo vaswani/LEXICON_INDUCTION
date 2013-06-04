6ef492c8beff99c79370428dbc96a8ff5d11de26
Ashish Vaswani <vaswani@usc.edu>
2013-06-03 23:24:12 -0400

    added code to generate data from the generative story. not complete

---
+++ SCRIPTS/generateData.py
@@ -0,0 +1,17 @@
+#generates the data from the generative story
+
+import sys
+import numpy
+import scipy
+
+class Words:
+    pass
+
+num_items = int(sys.argv[1])
+dim = int(sys.argv[2])
+epsilon = float(sys.argv[3])
+#q = float(  )
+X = Words();
+X.words = numpy.array(xrange(num_items))
+X.Z = numpy.random.multivariate_normal(numpy.zeros(dim),numpy.identity(dim),num_items)
+print X.Z
