cda595bf9ab89d1612764ced2aa33479997b770f
Ashish Vaswani <vaswani@usc.edu>
2013-06-10 00:33:16 -0400

    made a correction

--- newcode/calcScores.py
+++ newcode/calcScores.py
@@ -2,7 +2,8 @@
 import sys
 from collections import defaultdict
 
-#the dictionary is contains the list of all matching obtained from the gold lexicon. dictionary has format key, key of values
+#the dictionary is contains the list of all matching obtained from the gold lexicon. dictionary has format key, key of values.
+#source words is a dictionary of source words and target words is a dictionary of target words
 def filterDictionary(dictionary,source_words,target_words):
     filtered_dictionary = defaultict(defaultdict(string))
     total_items = 0
@@ -17,8 +18,8 @@ def filterDictionary(dictionary,source_words,target_words):
 
 
 #filtered dictionary is a dictioary with the key as a source word and the value is another dictionary with the key as the target words
-#matches is a list of lists. Each row in the list has th format [source_word,matched_target_word,score]. The items are sorted according to score
-def computePrecisionRecall(filtered_dictionary,matches,num_matches,dictionary_size,beta):
+#matches is a list of lists. Each row in the list has the format [source_word,matched_target_word,score]. The items are sorted according to score
+def computePrecisionRecall(filtered_dictionary,matches,dictionary_size,beta):
     recall_cutoffs=[0.05,0.1,0.2,0.25,0.33,0.4,0.5,0.6] 
     beta_squared = beta*beta
     recall_cutoff_index = 0
